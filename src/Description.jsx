23e59f7a813ff319eef6706304146c3552436f46
hatimaruuuu <hayata2504@gmail.com>
2024-03-30 20:28:31 +0900

    Co-authored-by: tak <taku10101@users.noreply.github.com> Co-authored-by: kigawa <kigawa01@users.noreply.github.com>

--- src/Description.jsx
+++ src/Description.jsx
@@ -1,7 +1,32 @@
-// @ts-check
+// @ts-nocheck
+
+import DogImage from './DogImage';
+import  { useState } from 'react';
 
 export const Description = () => {
-  return <></>
-}
+  const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/vizsla/n02100583_264.jpg',);
 
-export default Description
+  const handleClick = async () => {
+    try {
+      const res = await fetch('https://dog.ceo/api/breeds/image/random');
+
+      const data = await res.json();
+      setDogUrl(data.message); // stateを更新
+      console.log(data.message);
+      return data
+    } catch (err) {
+      console.log(err);
+
+    }
+  };
+
+  return (
+    <div>
+      <DogImage imageUrl={dogUrl} />
+      <button type="button" onClick={handleClick}>
+        更新
+      </button>
+    </div>
+  );
+};
+
